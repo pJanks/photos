2c9001ecffcb8cf654a4a5cc7d9df6dfff700472
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2024-08-18 01:01:07 +0000

    Refactor

--- generate_thumbnails.php
+++ generate_thumbnails.php
@@ -4,63 +4,66 @@
 
     $thumbnailsDirectory = __DIR__ . "/thumbnails";
     $photosDirectory = __DIR__ . "/photos";
-    $files = scandir($photosDirectory);
     $thumbWidth = 500;
     $html = "";
+
+    if (!is_dir($thumbnailsDirectory)) {
+        mkdir($thumbnailsDirectory, 0755, true);
+    }
 
     function createThumbnail($imagePath, $thumbnailPath, $thumbWidth) {
         $info = pathinfo($imagePath);
         $ext = strtolower($info["extension"]);
 
-        if ($ext === "jpg" || $ext === "jpeg") {
-            $img = imagecreatefromjpeg($imagePath);
-        } else if ($ext === "png") {
-            $img = imagecreatefrompng($imagePath);
-        } else if ($ext === "gif") {
-            $img = imagecreatefromgif($imagePath);
-        } else {
-            return false;
+        switch ($ext) {
+            case 'jpg':
+            case 'jpeg':
+                $img = imagecreatefromjpeg($imagePath);
+                break;
+            case 'png':
+                $img = imagecreatefrompng($imagePath);
+                break;
+            case 'gif':
+                $img = imagecreatefromgif($imagePath);
+                break;
+            default:
+                return false;
         }
 
         $width = imagesx($img);
         $height = imagesy($img);
         $thumbHeight = floor($height * ($thumbWidth / $width));
         $thumb = imagecreatetruecolor($thumbWidth, $thumbHeight);
-        
+
         imagecopyresampled($thumb, $img, 0, 0, 0, 0, $thumbWidth, $thumbHeight, $width, $height);
         imagejpeg($thumb, $thumbnailPath);
         imagedestroy($img);
         imagedestroy($thumb);
+        return true;
     }
 
+    $files = scandir($photosDirectory);
     foreach ($files as $file) {
         $filePath = "$photosDirectory/$file";
-        $extension = strtolower(pathinfo($filePath, PATHINFO_EXTENSION));
+        $extension = strtolower(pathinfo($file, PATHINFO_EXTENSION));
 
         if (is_file($filePath) && in_array($extension, ['jpg', 'jpeg', 'png', 'gif'])) {
             $thumbnailPath = "$thumbnailsDirectory/$file";
-            $thumbDir = dirname($thumbnailPath);
-
-            if (!is_dir($thumbDir)) {
-                mkdir($thumbDir, 0755, true);
-            }
             
-            if (!file_exists($thumbnailPath)) {
-                createThumbnail($filePath, $thumbnailPath, $thumbWidth);
-            }
+            if (!file_exists($thumbnailPath) && !createThumbnail($filePath, $thumbnailPath, $thumbWidth)) continue;
 
-            $relativeThumbnailPath = htmlspecialchars(str_replace(__DIR__, "", $thumbnailPath));
-            $relativeFilePath = htmlspecialchars(str_replace(__DIR__, "", $filePath));
+            $relativeThumbnailPath = str_replace(__DIR__, '', $thumbnailPath);
+            $relativeFilePath = str_replace(__DIR__, '', $filePath);        $relativeThumbnailPath = ltrim($relativeThumbnailPath, '/');
+            $relativeFilePath = ltrim($relativeFilePath, '/');
             $filename = htmlspecialchars(pathinfo($file, PATHINFO_FILENAME));
-            $sanitizedImage = htmlspecialchars($file);
 
             $html .= "<div class=\"card\">";
             $html .= "<a href=\"$relativeFilePath\" download>";
-            $html .= "<img src=\"$relativeThumbnailPath\" alt=\"$sanitizedImage\" loading=\"lazy\">";
+            $html .= "<img src=\"$relativeThumbnailPath\" alt=\"$filename\" loading=\"lazy\">";
             $html .= "</a>";
             $html .= "<span class=\"card-title\">$filename</span>";
             $html .= "</div>";
         }
     }
-
     echo $html;
+?>
